ff6e4d982e0dc86adf3b5c0e524ae25f564d82f6
Tarsis <br.tmvdl@gmail.com>
2024-01-08 17:44:15 -0300

    Updating app files

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,4 @@
+import { HTML } from '@brtmvdl/frontend'
 import { initializeApp } from "firebase/app"
 import { getAnalytics } from "firebase/analytics"
 
@@ -10,6 +11,11 @@ const firebaseConfig = {
   appId: "0:000000000000:web:00x0x00000xxx0000xx0x0",
   measurementId: "G-XX000X0XXX"
 }
-
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
+
+export class Page extends HTML {
+  onCreate() {
+    this.setText('Page')
+  }
+}
